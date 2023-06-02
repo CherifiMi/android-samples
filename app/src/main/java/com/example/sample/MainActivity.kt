7ac2fef102e39c20251be85468e37833f9dfc75e
CherifiMi <cherifi154949@gmail.com>
2023-06-02 12:33:16 +0100

    .

--- app/src/main/java/com/example/sample/MainActivity.kt
+++ app/src/main/java/com/example/sample/MainActivity.kt
@@ -12,6 +12,8 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.sample.ui.theme.SampleTheme
 
+// news
+
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
