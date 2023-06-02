5aeca8a68615c4380b4517e10dfc9951b0d6e4fe
CherifiMi <cherifi154949@gmail.com>
2023-06-02 21:07:47 +0100

    .

--- app/src/main/java/com/example/sample/MainActivity.kt
+++ app/src/main/java/com/example/sample/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             SampleTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
+                    Greeting("lagged")
                 }
             }
         }
