1d02c775cbd70ad46b4fdd56e047ac17b3d7d1c8
CherifiMi <cherifi154949@gmail.com>
2023-06-03 06:28:59 +0100

    lagged screen

--- app/src/main/java/com/example/sample/LaggedScreen.kt
+++ app/src/main/java/com/example/sample/LaggedScreen.kt
@@ -1,12 +1,24 @@
 package com.example.sample
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 
 
 @Composable
 fun LaggedScreen(){
-
+    Column(
+        Modifier
+            .fillMaxSize()
+            .background(Color.Blue)
+    ) {
+        Text(text = "Mitoooo")
+    }
 }
 
 @Preview
