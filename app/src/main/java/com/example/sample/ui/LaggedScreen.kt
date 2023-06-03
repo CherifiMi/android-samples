1caa8cc4934427ce905adb552f49d394539665dc
CherifiMi <cherifi154949@gmail.com>
2023-06-03 11:40:01 +0100

    .

--- app/src/main/java/com/example/sample/ui/LaggedScreen.kt
+++ app/src/main/java/com/example/sample/ui/LaggedScreen.kt
@@ -1,4 +1,4 @@
-package com.example.sample
+package com.example.sample.ui
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
@@ -6,15 +6,14 @@ import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.sample.SleepGraphData
 import com.example.sample.data.sleepData
+import com.example.sample.yellowBackground
 import java.time.LocalDateTime
 
 
