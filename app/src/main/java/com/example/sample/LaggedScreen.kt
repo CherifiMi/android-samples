e359c16e83625e629371617ba4dc9e042cce0e6e
CherifiMi <cherifi154949@gmail.com>
2023-06-03 04:55:07 +0100

    lagged screen

---
+++ app/src/main/java/com/example/sample/LaggedScreen.kt
@@ -0,0 +1,15 @@
+package com.example.sample
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+
+@Composable
+fun LaggedScreen(){
+
+}
+
+@Preview
+fun LaggedPreview(){
+    LaggedScreen()
+}
--- app/src/main/java/com/example/sample/MainActivity.kt
+++ app/src/main/java/com/example/sample/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             SampleTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("lagged")
+                    LaggedScreen()
                 }
             }
         }
@@ -29,15 +29,3 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    SampleTheme {
-        Greeting("Android")
-    }
-}
