4044e5f0401eae452ba94ace88873f8111bd1038
CherifiMi <cherifi154949@gmail.com>
2023-06-12 16:03:47 +0100

    .

--- app/src/main/java/com/example/sample/MainActivity.kt
+++ app/src/main/java/com/example/sample/MainActivity.kt
@@ -1,17 +1,24 @@
 package com.example.sample
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.core.withInfiniteAnimationFrameMillis
+import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.OffsetEffect
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.sample.theme.SampleTheme
-import com.example.sample.ui.LaggedScreen
+import com.example.sample.ui.theme.SampleTheme
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,11 +27,15 @@ class MainActivity : ComponentActivity() {
             SampleTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    LaggedScreen()
+                    Greeting("Android")
                 }
             }
         }
-
     }
 }
 
+@SuppressLint("CoroutineCreationDuringComposition")
+@Composable
+fun Greeting(name: String) {
+
+}
